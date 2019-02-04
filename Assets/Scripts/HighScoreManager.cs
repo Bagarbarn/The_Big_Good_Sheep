dd6d7ae4e29d6d1ec94a8cbc24cabc4203371e6b
Ella <ella.fernstrom@gmail.com>
2019-02-04 16:49:17 +0100

    ScoreBoard scene, file reading/writing

---
+++ Assets/Scripts/HighScoreManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+
+public class HighScoreManager : MonoBehaviour {
+
+    public Text[] highscoreSlots;
+
+    void Awake()
+    {
+        WriteString();
+    }
+
+    [MenuItem("Tools/Write file")]
+    static void WriteString()
+    {
+        string path = "Assets/highscore.txt";
+
+        StreamWriter writer = new StreamWriter(path, false);
+
+        for (int i = 0; i < 5; i++)
+        {
+            writer.WriteLine("Tommy");
+            writer.WriteLine("77");
+        }
+        writer.Close();
+
+        //Re-import the file to update the reference in the editor
+        AssetDatabase.ImportAsset(path);
+        TextAsset asset = (TextAsset)Resources.Load("highscore", typeof (TextAsset));
+
+        ReadString();
+    }
+
+    [MenuItem("Tools/Read file")]
+    static void ReadString()
+    {
+        string path = "Assets/highscore.txt";
+
+        StreamReader reader = new StreamReader(path);
+
+        while (reader.EndOfStream == false)
+        {
+            Debug.Log(reader.ReadLine());
+        }
+        
+        //reader.ReadToEnd();
+        reader.Close();
+    }
+}
