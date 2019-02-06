48f27a1a0f50050df69716a0ac1ccd4eb7825a6f
Ella <ella.fernstrom@gmail.com>
2019-02-06 17:13:54 +0100

    Finalized scoreboard

--- Assets/Scripts/HighScoreManager.cs
+++ Assets/Scripts/HighScoreManager.cs
@@ -29,19 +29,44 @@ public class HighScoreManager : MonoBehaviour {
     protected static List<HighScoreData> scoreList = new List<HighScoreData>();
     protected static string path = "Assets/highscore.txt";
     public static int playerScore;
+    public static string playerName;
+    
     public Text[] highscoreSlots;
+    public Text playerScoreSlot;
+    public InputField nameInput;
 
     void Awake()
     {
         playerScore = GameObject.FindGameObjectWithTag("ScoreHolder").GetComponent<ScoreHolderScript>().p_endScore;
+        playerScoreSlot.text = "Your score: " + playerScore;
+
         ReadFile();
-
-        for (int i = 0; i < highscoreSlots.Length; i++)
-        {
-            highscoreSlots[i].text = scoreList[i].GetName() + ": " + scoreList[i].GetScore();
-        }
-
+        UpdateList();
         WriteFile();
+    }
+
+    public void ReceiveSubmittedName()
+    {
+        playerName = nameInput.text;
+        HighScoreData newPlayerScore = new HighScoreData(playerName, playerScore);
+        scoreList.Add(newPlayerScore);
+        UpdateList();
+        WriteFile();
+    }
+
+    public void UpdateList()
+    {
+        scoreList.Sort((s1, s2) => -1 * s1.GetScore().CompareTo(s2.GetScore()));
+        Debug.Log(scoreList.Count);
+        if (highscoreSlots.Length <= scoreList.Count)
+        {
+            for (int i = 0; i < highscoreSlots.Length; i++){
+                highscoreSlots[i].text = scoreList[i].GetName() + ": " + scoreList[i].GetScore();}}
+        else {
+            for (int i = 0; i < scoreList.Count; i++){
+                highscoreSlots[i].text = scoreList[i].GetName() + ": " + scoreList[i].GetScore();
+            }
+        }
     }
 
     [MenuItem("Tools/Write file")]
@@ -79,16 +104,7 @@ public class HighScoreManager : MonoBehaviour {
                 scoreList.Add(newScore);
             }
         }
-        HighScoreData newPlayerScore = new HighScoreData("Player", playerScore);
-        scoreList.Add(newPlayerScore);
-
-        Debug.Log("--- COMPLETED LIST ---");
-
-        scoreList.Sort((s1, s2) => -1* s1.GetScore().CompareTo(s2.GetScore()));
-
-        for (int i = 0; i < scoreList.Count; i++){
-            Debug.Log(scoreList[i].GetName() + ": " + scoreList[i].GetScore());
-        }
+        Debug.Log("--- COMPLETED FILE READING ---");
         reader.Close();
     }
 }
