69927f2fe315f5db7af03182fb61f8989432957e
Mattias <Albert.vanderkolk.9509@student.uu.se>
2019-02-13 16:57:49 +0100

    Tutorial, completing the layout

--- Assets/Scripts/GameManager/TutorialManager.cs
+++ Assets/Scripts/GameManager/TutorialManager.cs
@@ -17,6 +17,9 @@ public class TutorialManager : MonoBehaviour {
     TutorialUIScript tutorialUI;
 
     Transform spawnPoint;
+    Transform offScreenPoint;
+
+    public int t_startEvent;
 
     public float tutorial_moveSpeed;
     public float tutorial_inwardMovement;
@@ -25,7 +28,14 @@ public class TutorialManager : MonoBehaviour {
 
     public GameObject background;
     public GameObject sheepObject;
+    public GameObject foxObject;
     public GameObject roadblockObject;
+
+    public GameObject overdriveObject;
+
+    public float tutorial_waitTime;
+
+    public float endTimer;
 
     private Vector2 topSpawn;
     private Vector2 bottomSpawn;
@@ -44,11 +54,39 @@ public class TutorialManager : MonoBehaviour {
         this.key_cancelColor = playerController.key_cancelColor;
 
         spawnPoint = GameObject.FindGameObjectWithTag("ObjectSpawner").transform;
+        offScreenPoint = GameObject.FindGameObjectWithTag("Offscreen").transform;
 
         topSpawn = new Vector2(spawnPoint.position.x, spawnPoint.position.y + spawnHeightDifference);
         bottomSpawn = new Vector2(spawnPoint.position.x, spawnPoint.position.y - spawnHeightDifference);
 
-        StartCoroutine("EventOne");
+        //Only for testing
+        StartRoutine();
+        //StartCoroutine("EventZero");
+    }
+
+
+    void StartRoutine()
+    {
+        switch (t_startEvent)
+        {
+            case 0: StartCoroutine("EventZero"); break;
+            case 1: StartCoroutine("EventOne"); break;
+            case 2: StartCoroutine("EventTwo"); break;
+            case 3: StartCoroutine("EventThree"); break;
+            case 4: StartCoroutine("EventFour"); break;
+            case 5: StartCoroutine("EventFive"); break;
+            case 6: StartCoroutine("EventSix"); break;
+            default: StartCoroutine("EventZero"); break;
+        }
+    }
+
+    IEnumerator EventZero()
+    {
+        t_startEvent++;
+
+        yield return null;
+
+        StartRoutine();
     }
 
 
@@ -56,15 +94,9 @@ public class TutorialManager : MonoBehaviour {
     IEnumerator EventOne()
     {
         Debug.Log("EventOne has started");
-
+        t_startEvent = 1;
         bool movedUp = false;
         bool movedDown = false;
-
-
-
-
-        //tutorialUI.ChangeActive(tutorialUI.keyBlue, true);
-        //tutorialUI.BlinkObject(tutorialUI.keyBlue, 6f);
 
         tutorialUI.BlinkObject(tutorialUI.moveSprites, 2f, 2f);
 
@@ -80,7 +112,7 @@ public class TutorialManager : MonoBehaviour {
         }
 
         //tutorialUI.ChangeActive(tutorialUI.moveSprites, false);
-        StartCoroutine("EventTwo");
+        StartCoroutine("EventZero");
     }
 
     //Single colored sheep;
@@ -130,7 +162,7 @@ public class TutorialManager : MonoBehaviour {
             yield return null;
         }
 
-        StartCoroutine("EventThree");
+        StartCoroutine("EventZero");
     }
 
 
@@ -176,7 +208,7 @@ public class TutorialManager : MonoBehaviour {
                 currentSheep.transform.Translate(Vector2.left * tutorial_moveSpeed * Time.deltaTime);
             yield return null;
         }
-
+        StartCoroutine("EventZero");
     }
 
     //The roadblock
@@ -184,8 +216,144 @@ public class TutorialManager : MonoBehaviour {
     {
         GameObject roadblock;
         roadblock = Instantiate(roadblockObject, spawnPoint.position, Quaternion.identity);
-        //while ()
-        yield return null;
+        //Get roadblock on the screen
+        while (roadblock.transform.position.x > tutorial_inwardMovement)
+        {
+            roadblock.transform.Translate(Vector2.left * tutorial_moveSpeed * Time.deltaTime);
+            yield return null;
+        }
+        float time = tutorial_waitTime;
+        tutorialUI.ChangeActive(tutorialUI.infoText, true);
+        tutorialUI.infoText.text = "A roadblock! Avoid it";
+        Vector2 lastPosrb = roadblock.transform.position;
+        while (time > 0)
+        {
+            time -= Time.deltaTime;
+            yield return null;
+        }
+        while (roadblock.transform.position.x > playerObject.transform.position.x)
+        {
+            if (roadblock.GetComponent<Tutorial_RoadblockScript>().playerHit)
+            {
+                roadblock.GetComponent<Tutorial_RoadblockScript>().playerHit = false;
+                Debug.Log("Roadblock hit");
+                tutorialUI.infoText.text = "You hit the roadblock, please try again";
+                roadblock.transform.position = lastPosrb;
+                float retime = tutorial_waitTime;
+                while (retime > 0)
+                {
+                    retime -= Time.deltaTime;
+                    yield return null;
+                }
+            }
+            roadblock.transform.Translate(Vector2.left * tutorial_moveSpeed * Time.deltaTime);
+            yield return null;
+        }
+        tutorialUI.infoText.text = "Well done!";
+        while (roadblock.transform.position.x > offScreenPoint.position.x)
+        {
+            roadblock.transform.Translate(Vector2.left * tutorial_moveSpeed * Time.deltaTime);
+            yield return null;
+        }
+        Destroy(roadblock);
+
+        StartCoroutine("EventZero");
     }
 
+    //Overdrive Pickup
+    IEnumerator EventFive()
+    {
+        GameObject pickup;
+        pickup = Instantiate(overdriveObject, topSpawn, Quaternion.identity);
+        while (pickup.transform.position.x > tutorial_inwardMovement)
+        {
+            pickup.transform.Translate(Vector2.left * tutorial_moveSpeed * Time.deltaTime);
+            yield return null;
+        }
+
+        tutorialUI.ChangeActive(tutorialUI.infoText, true);
+        tutorialUI.infoText.text = "A pickup! try running it over";
+
+        float time = tutorial_waitTime;
+
+        while (time > 0)
+        {
+            time -= Time.deltaTime;
+            yield return null;
+        }
+
+        bool pickedUp = false;
+
+        Vector2 lastPospu = pickup.transform.position;
+
+
+        while (!pickedUp)
+        {
+            if (pickup == null)
+            {
+                pickedUp = true;
+                tutorialUI.infoText.text = "Nice! Now serve the customers";
+            }
+            else
+            {
+                pickup.transform.Translate(Vector2.left * tutorial_moveSpeed * Time.deltaTime);
+
+                if (pickup.transform.position.x <= offScreenPoint.position.x)
+                {
+                    pickup.transform.position = lastPospu;
+                    tutorialUI.infoText.text = "You missed the pickup! Please run it over";
+                    float retime = tutorial_waitTime;
+                    while (retime > 0)
+                    {
+                        retime -= Time.deltaTime;
+                        yield return null;
+                    }
+                }
+
+            }
+            yield return null;
+        }
+
+        GameObject[] sheepArray = new GameObject[3];
+        sheepArray[0] = Instantiate(sheepObject, spawnPoint.position, Quaternion.identity);
+        sheepArray[0].GetComponent<Tutorial_SheepScript>().ObtainColor("orange");
+        sheepArray[1] = Instantiate(sheepObject, topSpawn, Quaternion.identity);
+        sheepArray[1].GetComponent<Tutorial_SheepScript>().ObtainColor("red");
+        sheepArray[2] = Instantiate(sheepObject, bottomSpawn, Quaternion.identity);
+        sheepArray[2].GetComponent<Tutorial_SheepScript>().ObtainColor("green");
+
+        while (sheepArray[0] != null || sheepArray[1] != null || sheepArray[2] != null)
+        {
+            for ( int i = 0; i < sheepArray.Length; i++)
+            {
+                if (sheepArray[i] != null)
+                {
+                    sheepArray[i].transform.Translate(Vector2.left * tutorial_moveSpeed * Time.deltaTime);
+                    if (sheepArray[i].transform.position.x <= offScreenPoint.position.x)
+                        Destroy(sheepArray[i]);
+                }
+            }
+
+            yield return null;
+        }
+
+        tutorialUI.ChangeActive(tutorialUI.infoText, false);
+        StartCoroutine("EventZero");
+    }
+
+    IEnumerator EventSix()
+    {
+        tutorialUI.ChangeActive(tutorialUI.timeText, true);
+        while (endTimer > 0)
+        {
+            tutorialUI.timeText.text = endTimer.ToString("F1");
+            endTimer -= Time.deltaTime;
+            yield return null;
+        }
+        endTimer = 0;
+        tutorialUI.timeText.text = endTimer.ToString("F1");
+        tutorialUI.ChangeActive(tutorialUI.infoText, true);
+        tutorialUI.infoText.text = "Well done! You completed the tutorial!";
+        //Set return to main menu button active
+    }
 }
---
+++ Assets/Tutorial_OverdriveScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tutorial_OverdriveScript : MonoBehaviour {
+
+    public float time;
+    public float overdriveShotSpeed;
+    private IEnumerator coroutine;
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            coroutine = other.gameObject.GetComponent<PlayerController>().Overdrive(time);
+            other.gameObject.GetComponent<PlayerController>().p_overdriveShotInterval = overdriveShotSpeed;
+            other.gameObject.GetComponent<PlayerController>().StartCoroutine(coroutine);
+            Destroy(this.gameObject);
+        }
+    }
+}
