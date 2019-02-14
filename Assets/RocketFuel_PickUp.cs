b6e14d4e52bef7ea8ba77fc69769e190646d3935
Mattias <Albert.vanderkolk.9509@student.uu.se>
2019-02-14 15:40:29 +0100

    Added two pickups

---
+++ Assets/RocketFuel_PickUp.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketFuel_PickUp : Obstacle {
+
+	public int p_effectTime;
+    public float p_boostPercentage;
+
+    private IEnumerator coroutine;
+
+    public override void ObstacleEvent(GameObject playerObject)
+    {
+        base.ObstacleEvent(playerObject);
+        playerObject.GetComponent<PlayerController>().p_boostPercentage = this.p_boostPercentage;
+        coroutine = playerObject.GetComponent<PlayerController>().SetBoost(p_effectTime);
+        playerObject.GetComponent<PlayerController>().StartCoroutine(coroutine);
+    }
+}
