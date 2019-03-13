4849cdff64939e095765ed610ceea4d528bd4297
Pontus Jakobsson <jakobsson.pontus@gmail.com>
2019-03-13 16:04:04 +0100

    Sound effect for power ups place holders, deleted unused old art files.

--- Assets/Scripts/PowerUps/InvincibilityScript.cs
+++ Assets/Scripts/PowerUps/InvincibilityScript.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class InvincibilityScript : Obstacle {
 
     public float duration;
-
+    SoundScript soundManager;
+    public AudioClip InvincibilitySound;
     private IEnumerator coroutine;
 
     public override void ObstacleEvent(GameObject playerObject)
@@ -14,6 +15,8 @@ public class InvincibilityScript : Obstacle {
         PlayerController pc = playerObject.GetComponent<PlayerController>();
         coroutine = pc.Invincibility(duration);
         pc.StartCoroutine(coroutine);
+        soundManager = GameObject.FindWithTag("SoundManager").GetComponent<SoundScript>();
+        soundManager.PlayAudio(InvincibilitySound);
     }
 
 }
