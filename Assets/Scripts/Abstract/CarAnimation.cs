d1733b5c2738b983697b1e8a8dd7ea0992e18d42
Ella <ella.fernstrom@gmail.com>
2019-03-20 16:33:59 +0100

    Player shoot animation

--- Assets/Scripts/Abstract/CarAnimation.cs
+++ Assets/Scripts/Abstract/CarAnimation.cs
@@ -4,26 +4,26 @@ using UnityEngine;
 
 public class CarAnimation : MonoBehaviour {
 
-    public float rotateSpeed = 10.0f;
+    //public float rotateSpeed = 10.0f;
 
     public float speed_max = .2f;
     public float speed_addition = 0.1f;
     float speed_current;
 
-    public GameObject wheel1;
-    public GameObject wheel2;
+    //public GameObject wheel1;
+    //public GameObject wheel2;
     public GameObject chassi;
 
-    private Transform wheel1_trans;
-    private Transform wheel2_trans;
+    //private Transform wheel1_trans;
+    //private Transform wheel2_trans;
     private Transform chassi_trans;
 
     private bool going_up;
 
     void Awake()
     {
-        wheel1_trans = wheel1.GetComponent<Transform>();
-        wheel2_trans = wheel2.GetComponent<Transform>();
+        //wheel1_trans = wheel1.GetComponent<Transform>();
+        //wheel2_trans = wheel2.GetComponent<Transform>();
         chassi_trans = chassi.GetComponent<Transform>();
         speed_current = speed_max;
         going_up = true;
@@ -31,8 +31,8 @@ public class CarAnimation : MonoBehaviour {
 
     void Update()
     {
-        wheel1_trans.Rotate(Vector3.forward * -rotateSpeed);
-        wheel2_trans.Rotate(Vector3.forward * -rotateSpeed);
+        //wheel1_trans.Rotate(Vector3.forward * -rotateSpeed);
+        //wheel2_trans.Rotate(Vector3.forward * -rotateSpeed);
         
         chassi_trans.Translate(Vector2.up * speed_current * Time.deltaTime);
         if (going_up == true)
