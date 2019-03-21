9d00f5dba21de1d23fb07e6e890ce5ff64486a38
Mattias <Albert.vanderkolk.9509@student.uu.se>
2019-03-21 12:04:38 +0100

    Rainbow particles work, start of end of the game

--- Assets/ParticleManager.cs
+++ Assets/ParticleManager.cs
@@ -5,7 +5,24 @@ using UnityEngine;
 public class ParticleManager : MonoBehaviour {
 
 
-    List<GameObject> activeSystems = new List<GameObject>();
+    public class ParticleEffect
+    {
+        public GameObject particleSystem;
+        public bool rainbow;
+        public bool colorsSet;
+        public ParticleEffect(GameObject part)
+        {
+            particleSystem = part;
+            rainbow = false;
+            colorsSet = false;
+        }
+    }
+
+
+
+    List<ParticleEffect> activeSystems = new List<ParticleEffect>();
+
+
 
     public void SpawnParticleSystem(GameObject system, Vector2 position, Color color)
     {
@@ -16,23 +33,32 @@ public class ParticleManager : MonoBehaviour {
             ParticleSystem particles = currentSystem.GetComponent<ParticleSystem>();
             var main = particles.main;
             gradient = main.startColor;
-            if (color != Color.black) main.startColor = color;
-            else main.startColor = main.startColor.Evaluate(Random.Range(0f, 1f));
-            particles.Play();
-            if (color == Color.black)
-            {
-                ParticleSystem.Particle[] parts = new ParticleSystem.Particle[particles.particleCount];
-                int num = particles.GetParticles(parts);
 
-                // Note: In theory and debug it changes the colors randomly but it is not visible in the game
-                for(int i = 0;  i < parts.Length; i++)
-                {
-                    float rand = Random.Range(0f, 1f);
-                    parts[i].startColor = gradient.Evaluate(rand);
-                }
-                particles.SetParticles(parts, num);
-            }
-            activeSystems.Add(currentSystem);
+
+            ParticleEffect partEffect = new ParticleEffect(currentSystem);
+            if (color != Color.black) main.startColor = color;
+            else { main.startColor = main.startColor.Evaluate(Random.Range(0f, 1f)); partEffect.rainbow = true; }
+            particles.Play();
+
+
+
+            //if (color == Color.black)
+            //{
+            //    ParticleSystem.Particle[] parts = new ParticleSystem.Particle[particles.particleCount];
+            //    int num = particles.GetParticles(parts);
+            //    Debug.Log(parts.Length);
+
+            //    // Note: In theory and debug it changes the colors randomly but it is not visible in the game
+            //    for(int i = 0;  i < parts.Length; i++)
+            //    {
+            //        Debug.Log("I'm doing it");
+            //        float rand = Random.Range(0f, 1f);
+            //        parts[i].startColor = main.startColor.Evaluate(Random.Range(0f, 1f));
+            //    }
+            //    Debug.Log("I can change the system!");
+            //    currentSystem.GetComponent<ParticleSystem>().SetParticles(parts, parts.Length);
+            //}
+            activeSystems.Add(partEffect);
         }
     }
 
@@ -40,10 +66,37 @@ public class ParticleManager : MonoBehaviour {
     {
         for (int i = 0; i < activeSystems.Count; i++)
         {
-            if(!activeSystems[i].GetComponent<ParticleSystem>().isPlaying)
+            if(!activeSystems[i].particleSystem.GetComponent<ParticleSystem>().isPlaying)
             {
-                Destroy(activeSystems[i]);
+                Destroy(activeSystems[i].particleSystem);
                 activeSystems.RemoveAt(i);
+            }
+        }
+    }
+
+    private void LateUpdate()
+    {
+
+        for (int i = 0; i < activeSystems.Count; i++)
+        {
+            if (activeSystems[i].rainbow && !activeSystems[i].colorsSet)
+            {
+                ParticleSystem particles = activeSystems[i].particleSystem.GetComponent<ParticleSystem>();
+                var main = particles.main;
+                ParticleSystem.Particle[] parts = new ParticleSystem.Particle[particles.particleCount];
+                int num = particles.GetParticles(parts);
+                Debug.Log(parts.Length);
+
+                // Note: In theory and debug it changes the colors randomly but it is not visible in the game
+                for (int j = 0; j < num; j++)
+                {
+                    Debug.Log("I'm doing it");
+                    float rand = Random.Range(0f, 1f);
+                    parts[j].startColor = particles.customData.GetColor(0).Evaluate(rand);
+                }
+                Debug.Log("I can change the system!");
+                activeSystems[i].particleSystem.GetComponent<ParticleSystem>().SetParticles(parts, parts.Length);
+                activeSystems[i].colorsSet = true;
             }
         }
     }
